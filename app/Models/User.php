05c0216dffbf9ad94e16fb9246a88bb00578b0ac
Swapnil Bhavsar <swapnil@chitranu.com>
2021-01-02 16:32:16 +0530

    ⚡ Configure User model to use Billable trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Cashier\Billable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
