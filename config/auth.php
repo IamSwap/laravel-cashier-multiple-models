562cb0108cb806538cfafa6193de38a29bf83bab
Swapnil Bhavsar <swapnil@chitranu.com>
2021-01-02 16:33:44 +0530

    🔧 Register auth guard & providers for the model Seller

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+        'seller' => [
+            'driver' => 'session',
+            'provider' => 'sellers',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -69,6 +74,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+
+        'sellers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Seller::class,
         ],
 
         // 'users' => [
